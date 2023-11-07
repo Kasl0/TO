e8f49d960da7cfadb0a6e89b47b9857e9b5784d9
Kasl0 <slonieckacper@gmail.com>
2023-11-07 18:09:52 +0100

    Lab6 data persistence

---
+++ Lab6/src/main/java/pl/edu/agh/to/school/student/Student.java
@@ -0,0 +1,50 @@
+package pl.edu.agh.to.school.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String indexNumber;
+
+    public Student(String firstName, String lastName, LocalDate birthDate, String indexNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.indexNumber = indexNumber;
+    }
+
+    public Student() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getIndexNumber() {
+        return indexNumber;
+    }
+}
